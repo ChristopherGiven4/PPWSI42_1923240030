492daace92d5fb6cb70e7026160cfa571b327afc
Christopher Given <christophergiven8@gmail.com>
2021-06-09 14:59:58 +0700

    Membuat Authentication dan Mengunakan API Token dengan Sanctum

--- routes/web.php
+++ routes/web.php
@@ -52,3 +52,7 @@ Route::get('/mahasiswa/update-elq', [MahasiswaController::class, 'updateElq']);
 Route::get('/mahasiswa/delete-elq', [MahasiswaController::class, 'deleteElq']);
 Route::get('/mahasiswa/select-elq', [MahasiswaController::class, 'selectElq']);
 
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
